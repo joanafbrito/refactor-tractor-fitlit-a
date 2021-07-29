27f0908f18d616e9be3312c82aec58b37a64746e
Janika Hortizuela <jhortizuela21@gmail.com>
2021-07-28 20:49:09 -0600

    Refactor hydration-test.js to include arrow functions

--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -4,7 +4,7 @@ import Hydration from '../src/Hydration';
 import UserRepository from '../src/UserRepository';
 import User from '../src/User';
 
-describe('Hydration', function() {
+describe('Hydration', () => {
   let hydration;
   let user1;
   let user2;
@@ -59,26 +59,26 @@ describe('Hydration', function() {
       "numOunces": 91
     }, userRepository)
   })
-  it('should be a function', function() {
+  it('should be a function', () => {
     expect(Hydration).to.be.a('function');
   });
-  it('should be an instance of hydrate', function() {
+  it('should be an instance of hydrate', () => {
     expect(hydrate1).to.be.an.instanceof(Hydration);
   });
-  it('should have an id', function() {
+  it('should have an id', () => {
     expect(hydrate2.userId).to.equal(2);
   });
-  it('should have a date', function() {
+  it('should have a date', () => {
     expect(hydrate3.date).to.equal('2019/06/16');
   });
-  it('should have an amount of ounces drank', function() {
+  it('should have an amount of ounces drank', () => {
     expect(hydrate3.ounces).to.equal(91);
   });
-  describe('drink', function () {
-    it('should update the average number of ounces over all time', function() {
+  describe('drink', () => {
+    it('should update the average number of ounces over all time', () => {
       expect(user2.ouncesAverage).to.equal(83);
     })
-    it('should add the date and amount to the object record', function() {
+    it('should add the date and amount to the object record', () => {
       expect(user1.ouncesRecord).to.deep.equal([{"2019/06/15": 37}])
       expect(user2.ouncesRecord.length).to.equal(2)
     })
