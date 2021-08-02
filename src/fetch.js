4f6aee6bbbda18d5ea0d9d9f902fa0343eab22d4
Joana Brito <joanafiorentini@hotmail.com>
2021-08-01 18:16:02 -0700

    Add fetch request for all data types

---
+++ src/fetch.js
@@ -0,0 +1,31 @@
+
+
+let userData, hydrationData, sleepData, activityData
+
+// Data Fetching 
+let fetchData = (dataType) => {
+    console.log(`http://localhost:3001/api/v1/${dataType}`)
+    return fetch(`http://localhost:3001/api/v1/${dataType}`)
+        .then(response => {
+            return response.ok ? response.json() : console.log(`ERROR with ${dataType} path`)
+        })
+        .then(data => data)
+}
+
+
+Promise.all([fetchData('users'), fetchData('hydration'), fetchData('sleep'), fetchData('activity')]).then((data)=> {
+    updateData(data)
+})
+
+let userRepository = new UserRepository();
+
+let updateData = (data) => {
+    userData = data[0].userData
+    hydrationData = data[1].hydrationData
+    sleepData = data[2].sleepData
+    activityData = data[3].activityData
+    // call a function that wraps everything from the dom
+}
+
+
+// export default userFetch;
