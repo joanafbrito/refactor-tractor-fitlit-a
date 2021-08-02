5d147b85dee83989bccdb814ea7560c319975a5c
Joana Brito <joanafiorentini@hotmail.com>
2021-08-02 13:06:41 -0700

    Move query selectors to the top

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -5,35 +5,6 @@ import Activity from './Activity';
 import Hydration from './Hydration';
 import Sleep from './Sleep';
 
-
-let userRepository = new UserRepository();
-
-// from the Server
-let userData, hydrationData, sleepData, activityData
-
-// Data Fetching 
-let fetchData = (dataType) => {
-    return fetch(`http://localhost:3001/api/v1/${dataType}`)
-        .then(response => {
-            return response.ok ? response.json() : console.log(`ERROR with ${dataType} path`)
-        })
-        .then(data => data)
-}
-
-// Promises 
-Promise.all([fetchData('users'), fetchData('hydration'), fetchData('sleep'), fetchData('activity')]).then((data)=> {
-    updateData(data)
-})
-
-let updateData = (data) => {
-    userData = data[0].userData
-    hydrationData = data[1].hydrationData
-    sleepData = data[2].sleepData
-    activityData = data[3].activityData
-    // call a function that updates the dom
-    updateDom()
-}
-
 // Query Selectors 
 let friendsStepsParagraphs = document.querySelectorAll('.friends-steps');
 let dailyOz = document.querySelectorAll('.daily-oz');
@@ -92,6 +63,33 @@ let trendingStepsPhraseContainer = document.querySelector('.trending-steps-phras
 let trendingStairsPhraseContainer = document.querySelector('.trending-stairs-phrase-container');
 let userInfoDropdown = document.querySelector('#user-info-dropdown');
 
+let userRepository = new UserRepository();
+
+// from the Server
+let userData, hydrationData, sleepData, activityData
+
+// Data Fetching 
+let fetchData = (dataType) => {
+    return fetch(`http://localhost:3001/api/v1/${dataType}`)
+        .then(response => {
+            return response.ok ? response.json() : console.log(`ERROR with ${dataType} path`)
+        })
+        .then(data => data)
+}
+
+// Promises 
+Promise.all([fetchData('users'), fetchData('hydration'), fetchData('sleep'), fetchData('activity')]).then((data)=> {
+    updateData(data)
+})
+
+let updateData = (data) => {
+    userData = data[0].userData
+    hydrationData = data[1].hydrationData
+    sleepData = data[2].sleepData
+    activityData = data[3].activityData
+    // call a function that updates the dom
+    updateDom()
+}
 
 const updateDom = () => {
 
